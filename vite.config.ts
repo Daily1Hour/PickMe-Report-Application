51228f5ccc5ef9418ba0515ba11ae833d99721cd
NarciSource <jwch11@gmail.com>
2024-12-15 16:20:32 +0900

    init Vue 3 프로젝트 초기화

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig, loadEnv } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 import tsconfigPaths from "vite-tsconfig-paths";
 
@@ -13,6 +14,7 @@ export default defineConfig(({ mode }) => {
     // vite 설정
     return {
         plugins: [
+            vue(),
             tsconfigPaths(), // tsconfig.json의 paths 설정을 적용
         ],
         build: {
