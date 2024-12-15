2e4351782b76b5b95fcd58644ae7c564c491aaa8
NarciSource <jwch11@gmail.com>
2024-12-15 17:52:07 +0900

    feat: 카드, 에디터 컴포넌트 추가

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,27 +1,20 @@
-<script setup lang="ts"></script>
-
 <template>
-    <div>
-        <a href="https://vite.dev" target="_blank">
-            <img src="/vite.svg" class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://vuejs.org/" target="_blank">
-            <img src="/vue.svg" class="logo vue" alt="Vue logo" />
-        </a>
-    </div>
+  <div class="q-pa-md">
+    <q-expansion-item v-model="expanded" :label="label" :caption="caption">
+      <q-card>
+        <q-card-section>
+          <q-editor v-model="editor" :definitions="{ bold: { label: 'Bold' } }" />
+        </q-card-section>
+      </q-card>
+    </q-expansion-item>
+  </div>
 </template>
 
-<style scoped>
-.logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-}
-.logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-    filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script setup>
+import { ref } from "vue";
+
+const expanded = ref(true);
+const editor = ref("에디터");
+const caption = ref("캡션");
+const label = ref("타이틀");
+</script>
--- src/app/main.ts
+++ src/app/main.ts
@@ -3,4 +3,7 @@ import { Quasar } from "quasar";
 
 import App from "./App.vue";
 
+import "quasar/src/css/index.sass";
+import "@quasar/extras/material-icons/material-icons.css";
+
 createApp(App).use(Quasar).mount("#app");
