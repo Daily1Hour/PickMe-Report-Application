845b83d9434c576a74aa00740af5207db951d6a6
NarciSource <jwch11@gmail.com>
2024-12-17 21:24:57 +0900

    feat: dto 추가

--- src/pages/reports/api/dto.ts
+++ src/pages/reports/api/dto.ts
@@ -2,12 +2,18 @@ export interface ReportDTO {
   category: string;
   createdAt: Date;
   updatedAt: Date;
-  companyDetails: CompanyDetailDTO[];
+  companyDetails: (CompanyDetailDTO | IndustryDetailDTO)[];
 }
 
-interface CompanyDetailDTO {
+export interface CompanyDetailDTO {
   companyName: string;
   companyFeatures: string;
   companyIdealTalent: string;
   companyNews: string;
+}
+
+export interface IndustryDetailDTO {
+  industryType: string;
+  industryFeatures: string;
+  industryNews: string;
 }
