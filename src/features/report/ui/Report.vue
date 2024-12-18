16bd177fa0d8c5dac9ea0b9dd56e812e8a614b09
NarciSource <jwch11@gmail.com>
2024-12-18 15:42:56 +0900

    refactor: 책임성 명확을 위해 데이터 페칭을 Report 컴포넌트로 옮김

--- src/features/report/ui/Report.vue
+++ src/features/report/ui/Report.vue
@@ -12,15 +12,62 @@
 </template>
 
 <script setup lang="ts">
+import { ref, watchEffect } from "vue";
 import Section from "./Section.vue";
 
-import { ReportType } from "@/entities/report/model";
+import { CompanyReport, IndustryReport, ReportType } from "@/entities/report/model";
+import client from "@/shared/api/client";
+import { ReportDTO, CompanyDetailDTO, IndustryDetailDTO } from "@/shared/api/dto";
 
 const props = defineProps<{
-  report: ReportType;
+  category: string;
+  created_at: string;
 }>();
 
+const report_keys = ref<string[]>([]);
+const report = ref<ReportType>({
+  name: "",
+  type: "",
+  features: "",
+  ideal_talent: "",
+  news: "",
+});
+
+watchEffect(async () => {
+  const data = await client.get<ReportDTO>("", {
+    params: {
+      category: props.category,
+      createdAt: props.created_at,
+    },
+  });
+
+  if (data.status === 200) {
+    const category = data.data.category;
+    const fetch_report = data.data.companyDetails[0];
+
+    switch (category) {
+      case "company":
+        report.value = new CompanyReport(
+          (fetch_report as CompanyDetailDTO).companyName,
+          (fetch_report as CompanyDetailDTO).companyFeatures,
+          (fetch_report as CompanyDetailDTO).companyIdealTalent,
+          (fetch_report as CompanyDetailDTO).companyNews,
+        );
+        break;
+      case "industry":
+        report.value = new IndustryReport(
+          (fetch_report as IndustryDetailDTO).industryType,
+          (fetch_report as IndustryDetailDTO).industryFeatures,
+          (fetch_report as IndustryDetailDTO).industryNews,
+        );
+        break;
+    }
+
+    report_keys.value = Object.keys(report.value);
+  }
+});
+
 function save() {
-  console.log("report", props.report);
+  console.log("report", report.value);
 }
 </script>
