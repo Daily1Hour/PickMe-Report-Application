2f0082e310f25b6526f7415960db459af16fcbc2
NarciSource <jwch11@gmail.com>
2024-12-19 09:44:15 +0900

    feat: Quasar 및 라우터 통합을 위한 앱 인스턴스 설정 변경

--- src/app/application.ts
+++ src/app/application.ts
@@ -1,19 +1,27 @@
-import { createApp, h } from "vue";
 import singleSpaVue from "single-spa-vue";
+import { createApp, h } from "vue";
+import { Quasar } from "quasar";
 
+import router from "./router";
 import App from "./App.vue";
 
+import "quasar/src/css/index.sass";
+import "@quasar/extras/material-icons/material-icons.css";
+
 const vueLifecycles = singleSpaVue({
-    createApp,
-    appOptions: {
-        render() {
-            return h(App, {
-                props: {
-                    name: this.name,
-                },
-            });
+  createApp,
+  appOptions: {
+    render() {
+      return h(App, {
+        props: {
+          name: this.name,
         },
+      });
     },
+  },
+  handleInstance: (app) => {
+    app.use(router).use(Quasar);
+  },
 });
 
 export const bootstrap = vueLifecycles.bootstrap;
