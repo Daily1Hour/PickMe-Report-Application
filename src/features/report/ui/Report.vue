4d9239dc0520051a093853f33ebc83e704eda717
NarciSource <jwch11@gmail.com>
2024-12-19 23:34:22 +0900

    refactor: section-form 컴포넌트 이름 변경

--- src/features/report/ui/Report.vue
+++ src/features/report/ui/Report.vue
@@ -8,7 +8,7 @@
   <div class="column" style="width: calc(100% - 150px)">
     <ul style="list-style-type: none; order: 1">
       <li v-for="key in titles" :key="key">
-        <section-tab :id="key" v-model:content="report[key as keyof ReportType]" />
+        <section-form :id="key" v-model:content="report[key as keyof ReportType]" />
       </li>
     </ul>
     <div class="row justify-end" v-for="order in [0, 2]" :key="order" :style="{ order }">
@@ -21,7 +21,7 @@
 import { ref } from "vue";
 
 import ReadReport from "./read-report.vue";
-import SectionTab from "./section-tab.vue";
+import SectionForm from "./section-form.vue";
 import SaveReport from "./save-report.vue";
 import { map_to_companyReport, map_to_industryReport } from "../api/mapper";
 import { ReportType } from "@/entities/report/model";
