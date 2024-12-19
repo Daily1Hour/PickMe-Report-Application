db33038265987dd0de290c854fb2e62d95a00c1c
NarciSource <jwch11@gmail.com>
2024-12-19 23:31:49 +0900

    refactor: read-report 컴포넌트로 데이터 요청 로직 분리

--- src/features/report/ui/Report.vue
+++ src/features/report/ui/Report.vue
@@ -1,4 +1,10 @@
 <template :key="$route.fullPath">
+  <read-report
+    :category="props.category"
+    :created_at="props.created_at"
+    @fetched="report = $event"
+  />
+
   <div class="column" style="width: calc(100% - 150px)">
     <ul style="list-style-type: none; order: 1">
       <li v-for="key in titles" :key="key">
@@ -13,64 +19,25 @@
 
 <script setup lang="ts">
 import { ref } from "vue";
-import { useRoute } from "vue-router";
 
+import ReadReport from "./read-report.vue";
 import SectionTab from "./section-tab.vue";
 import SaveReport from "./save-report.vue";
-import { ReportDTO, CompanyDetailDTO, IndustryDetailDTO } from "../api/dto";
 import { map_to_companyReport, map_to_industryReport } from "../api/mapper";
 import { ReportType } from "@/entities/report/model";
 import { Category } from "@/shared/model/Category";
-import client from "@/shared/api/client";
 
 const props = defineProps<{
   category: Category;
   created_at: Date;
 }>();
 
-const route = useRoute();
-const titles = ref<string[]>();
+const titles = ref<string[]>(
+  props.category === Category.Company
+    ? ["name", "features", "ideal_talent", "news"]
+    : ["type", "features", "news"],
+);
 const report = ref<ReportType>(
   props.category === Category.Company ? map_to_companyReport() : map_to_industryReport(),
 );
-
-titles.value =
-  props.category === Category.Company
-    ? ["name", "features", "ideal_talent", "news"]
-    : ["type", "features", "news"];
-
-if (route.name === "new") {
-  report.value =
-    props.category === Category.Company ? map_to_companyReport() : map_to_industryReport();
-} else if (route.name === "detail") {
-  fetch();
-}
-
-async function fetch() {
-  const params = {
-    category: props.category,
-    createdAt: props.created_at.toISOString(),
-  };
-
-  const data = await client.get<ReportDTO>("", { params });
-
-  if (data.status === 200) {
-    const updated_category = data.data.category;
-
-    switch (updated_category) {
-      case Category.Company: {
-        const fetch_report = data.data.companyDetails?.[0];
-        report.value = map_to_companyReport(fetch_report as CompanyDetailDTO);
-        break;
-      }
-      case Category.Industry: {
-        const fetch_report = data.data.industryDetails?.[0];
-        report.value = map_to_industryReport(fetch_report as IndustryDetailDTO);
-        break;
-      }
-    }
-
-    titles.value = Object.keys(report.value) as string[];
-  }
-}
 </script>
---
+++ src/features/report/ui/read-report.vue
@@ -0,0 +1,52 @@
+<template></template>
+
+<script setup lang="ts">
+import { useRoute } from "vue-router";
+
+import { ReportDTO, CompanyDetailDTO, IndustryDetailDTO } from "../api/dto";
+import { map_to_companyReport, map_to_industryReport } from "../api/mapper";
+import { Category } from "@/shared/model/Category";
+import client from "@/shared/api/client";
+
+const props = defineProps<{
+  category: Category;
+  created_at: Date;
+}>();
+
+const emit = defineEmits(["fetched"]);
+
+const route = useRoute();
+
+if (route.name === "new") {
+  emit(
+    "fetched",
+    props.category === Category.Company ? map_to_companyReport() : map_to_industryReport(),
+  );
+} else if (route.name === "detail") {
+  emit("fetched", await fetch());
+}
+
+async function fetch() {
+  const params = {
+    category: props.category,
+    createdAt: props.created_at.toISOString(),
+  };
+
+  const data = await client.get<ReportDTO>("", { params });
+
+  if (data.status === 200) {
+    const updated_category = data.data.category;
+
+    switch (updated_category) {
+      case Category.Company: {
+        const fetch_report = data.data.companyDetails?.[0];
+        return map_to_companyReport(fetch_report as CompanyDetailDTO);
+      }
+      case Category.Industry: {
+        const fetch_report = data.data.industryDetails?.[0];
+        return map_to_industryReport(fetch_report as IndustryDetailDTO);
+      }
+    }
+  }
+}
+</script>
