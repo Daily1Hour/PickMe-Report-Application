fc7d62777245f939076e188de4de3cc0de7ed320
NarciSource <jwch11@gmail.com>
2024-12-22 18:32:24 +0900

    feat: EditorJS 컴포넌트 추가

--- src/features/report/ui/section-form.vue
+++ src/features/report/ui/section-form.vue
@@ -3,7 +3,17 @@
     <q-expansion-item v-model="expanded" :label="label" :caption="label + '을 입력해주세요.'">
       <q-card>
         <q-card-section>
-          <q-editor v-model="store.report[key]" :definitions="{ bold: { label: 'Bold' } }" />
+          <q-editor
+            v-if="just_text"
+            v-model="store.report[key]"
+            :definitions="{ bold: { label: 'Bold' } }"
+          />
+          <editorjs
+            v-else
+            placeholder="빈칸"
+            :modelValue="store.report[key]"
+            @update="update"
+          />
         </q-card-section>
       </q-card>
     </q-expansion-item>
@@ -12,6 +22,8 @@
 
 <script setup lang="ts">
 import { computed, ref } from "vue";
+
+import editorjs from "@/widgets/editorjs.vue";
 
 import { useReportStore } from "../store/report";
 import { ReportType } from "@/entities/report/model";
@@ -25,4 +37,9 @@ const store = useReportStore();
 const expanded = ref(true);
 const key = computed(() => props.id as keyof ReportType);
 const label = computed(() => sections_map[key.value]);
+const just_text = computed(() => key.value === "name" || key.value === "type");
+
+const update = (value: any) => {
+  store.report[key.value] = JSON.stringify(value);
+};
 </script>
---
+++ src/widgets/editorjs.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="editorjs" ref="editor_container"></div>
+</template>
+
+<script setup>
+import { onMounted, onUnmounted, ref, watch } from "vue";
+import EditorJS from "@editorjs/editorjs";
+
+const editor_container = ref(null);
+
+const props = defineProps(["modelValue", "placeholder"]);
+const emit = defineEmits(["update"]);
+
+let editor;
+let updating_model = false;
+
+function modelToView() {
+  const parsed = JSON.parse(props.modelValue || "{}");
+
+  if (props.modelValue) {
+    editor.render(parsed);
+  }
+}
+
+function viewToModel(api, event) {
+  updating_model = true;
+
+  editor
+    .save()
+    .then((data) => emit("update", data))
+    .catch((error) => {
+      console.log(event, "Saving failed: ", error);
+    })
+    .finally(() => {
+      updating_model = false;
+    });
+}
+
+onMounted(() => {
+  editor = new EditorJS({
+    holder: editor_container.value,
+    placeholder: props.placeholder,
+    inlineToolbar: ["bold", "italic", "link"],
+    tools: {},
+    minHeight: "auto",
+    data: JSON.parse(props.modelValue || "{}"),
+    onReady: modelToView,
+    onChange: viewToModel,
+  });
+});
+
+watch(
+  () => props.modelValue,
+  () => {
+    if (!updating_model) {
+      modelToView();
+    }
+  },
+);
+
+onUnmounted(() => {
+  //editor.destroy();
+});
+</script>
