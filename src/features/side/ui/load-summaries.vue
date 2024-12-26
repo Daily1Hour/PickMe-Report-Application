44c718d832e63f6873b3c4008ee0f25ae48bf712
NarciSource <jwch11@gmail.com>
2024-12-26 21:42:52 +0900

    refactor: getSummaries API 호출 개선

--- src/features/side/ui/load-summaries.vue
+++ src/features/side/ui/load-summaries.vue
@@ -2,29 +2,25 @@
 
 <script setup lang="ts">
 import { watch } from "vue";
-import { useQueries } from "@tanstack/vue-query";
+import { useQuery } from "@tanstack/vue-query";
 
 import { getSummaries } from "../api";
 import { useSummaryStore } from "../store/summary";
-import { Summary } from "@/entities/summary/model";
-import { Category } from "@/shared/model/Category";
 import { QueryKey } from "@/shared/model/QueryKey";
 
-const emit = defineEmits(["fetched"]);
-
+// 상태 저장소
 const store = useSummaryStore();
 
-const data = useQueries({
-  queries: [Category.Company, Category.Industry].map((category) => ({
-    queryKey: [QueryKey.Summaries, category],
-    queryFn: getSummaries,
-    initialData: [],
-    retry: false,
-  })),
-  combine: (results) => results.map((result) => result.data).flat() as Summary[],
+// 쿼리
+const { data } = useQuery({
+  queryKey: [QueryKey.Summaries],
+  queryFn: getSummaries,
+  initialData: [],
+  retry: false,
 });
 
-watch(data, (queries) => {
-  store.summaries = queries;
+// 페칭 완료 후 상태 저장
+watch(data, (summaries) => {
+  store.summaries = summaries;
 });
 </script>
