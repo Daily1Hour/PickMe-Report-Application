ed4d0b8db9b79dec155835c62eb516bbf8fa843d
NarciSource <jwch11@gmail.com>
2024-12-26 21:56:42 +0900

    refactor: DisplaySummaries 컴포넌트 분리

--- src/features/side/index.vue
+++ src/features/side/index.vue
@@ -3,59 +3,11 @@
 
   <q-fab persistent color="secondary" icon="menu" direction="down" style="height: 50px">
     <q-fab-action style="transform: translateX(50%); background: white">
-      <div class="q-pa-md" style="max-width: 350px; position: sticky; top: 0; height: 100%">
-        <q-list bordered separator>
-          <q-item clickable v-ripple>
-            <add-report />
-          </q-item>
-
-          <q-item
-            v-for="summary in pagination_list"
-            clickable
-            v-ripple
-            :key="updated_time(summary)"
-          >
-            <section-tab :summary="summary" />
-            <remove-report :id="summary.id" />
-          </q-item>
-        </q-list>
-
-        <div class="flex flex-center">
-          <q-pagination
-            v-model="current"
-            :max="pagination_max"
-            input
-            @click="(event: MouseEvent) => event.stopPropagation()"
-          />
-        </div>
-      </div>
+      <display-summaries />
     </q-fab-action>
   </q-fab>
 </template>
 
 <script setup lang="ts">
-import { ref, computed } from "vue";
-
-import { SectionTab, LoadSummaries, AddReport, RemoveReport } from "./ui";
-import { useSummaryStore } from "./store/summary";
-import { Summary } from "@/entities/summary/model";
-
-const store = useSummaryStore();
-
-const sorted_summaries = computed(() =>
-  store.summaries.sort((a, b) => b.created_at.getTime() - a.created_at.getTime()),
-);
-
-const current = ref(1);
-
-const pagination_max = computed(() => store.summaries.length / 5 + 1);
-
-const pagination_list = computed(() => {
-  const start = (current.value - 1) * 5;
-  const end = current.value * 5;
-
-  return sorted_summaries.value.slice(start, end);
-});
-
-const updated_time = (summary: Summary) => summary.updated_at.toISOString();
+import { LoadSummaries, DisplaySummaries } from "./ui";
 </script>
---
+++ src/features/side/ui/display-summaries.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="q-pa-md" style="max-width: 350px; position: sticky; top: 0; height: 100%">
+    <q-list bordered separator>
+      <q-item clickable v-ripple>
+        <add-report />
+      </q-item>
+
+      <q-item v-for="summary in pagination_list" clickable v-ripple :key="updated_time(summary)">
+        <section-tab :summary="summary" />
+        <remove-report :id="summary.id" />
+      </q-item>
+    </q-list>
+
+    <div class="flex flex-center">
+      <q-pagination
+        v-model="current"
+        :max="pagination_max"
+        input
+        @click="(event: MouseEvent) => event.stopPropagation()"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, computed } from "vue";
+
+import { SectionTab, AddReport, RemoveReport } from "../ui";
+import { useSummaryStore } from "../store/summary";
+import { Summary } from "@/entities/summary/model";
+
+const store = useSummaryStore();
+
+const sorted_summaries = computed(() =>
+  store.summaries.sort((a, b) => b.created_at.getTime() - a.created_at.getTime()),
+);
+
+const current = ref(1);
+
+const pagination_max = computed(() => store.summaries.length / 5 + 1);
+
+const pagination_list = computed(() => {
+  const start = (current.value - 1) * 5;
+  const end = current.value * 5;
+
+  return sorted_summaries.value.slice(start, end);
+});
+
+const updated_time = (summary: Summary) => summary.updated_at.toISOString();
+</script>
