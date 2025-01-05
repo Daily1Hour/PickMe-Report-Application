1a46a406d261db3ca5c2b20426033bf7d666563b
NarciSource <jwch11@gmail.com>
2025-01-05 14:52:24 +0900

    feat: 보고서 삭제 후 상태 초기화 및 홈으로 이동

--- src/features/report/ui/remove-report.vue
+++ src/features/report/ui/remove-report.vue
@@ -18,11 +18,17 @@
 
 <script setup lang="ts">
 import { ref } from "vue";
+import { useRouter } from "vue-router";
 import { useMutation, useQueryClient } from "@tanstack/vue-query";
 
 import { deleteReport } from "../api";
+import { useReportStore } from "../store/report";
+import { ReportType } from "@/entities/report/model";
 import { QueryKey } from "@/shared/model/QueryKey";
+import { RouteName } from "@/shared/model/RouteName";
 
+const router = useRouter();
+const store = useReportStore();
 const props = defineProps<{ id: string }>();
 const confirm = ref(false);
 
@@ -31,8 +37,14 @@ const queryClient = useQueryClient();
 const mutation = useMutation({
   mutationFn: () => deleteReport(props.id),
   onSuccess: () => {
+    // 상태 저장소 초기화
+    store.report = {} as ReportType;
+
     // 삭제 후 사이드 목록 갱신
     queryClient.refetchQueries({ queryKey: [QueryKey.Summaries] });
+
+    // 홈으로 이동
+    router.push({ name: RouteName.Home });
   },
 });
 </script>
