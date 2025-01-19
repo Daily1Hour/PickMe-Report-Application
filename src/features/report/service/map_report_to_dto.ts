08a368d51be6f8f323a3b6b9b89ad1efd0fe2167
NarciSource <jwch11@gmail.com>
2025-01-19 09:07:33 +0900

    refactor: setReport의 DTO ↔ 엔터티모델 변환 시점을 변경

---
+++ src/features/report/service/map_report_to_dto.ts
@@ -0,0 +1,32 @@
+import { CompanyDetailDTO, IndustryDetailDTO } from "../api/dto";
+import { CompanyReport, IndustryReport } from "@/entities/report/model";
+import { Category } from "@/shared/model/Category";
+
+export default (category: Category, report: CompanyReport | IndustryReport) => {
+  if (report.hasOwnProperty("name")) {
+    const { id, name, features, ideal_talent, news } = report as CompanyReport;
+    report = new CompanyReport(id, name, features, ideal_talent, news);
+  } else {
+    const { id, type, features, news } = report as IndustryReport;
+    report = new IndustryReport(id, type, features, news);
+  }
+
+  return {
+    category,
+    companyDetail: report instanceof CompanyReport ? map_to_companyDetailDTO(report) : undefined,
+    industryDetail: report instanceof IndustryReport ? map_to_industryDetailDTO(report) : undefined,
+  };
+};
+
+const map_to_companyDetailDTO = (report: CompanyReport): CompanyDetailDTO => ({
+  companyName: report.name,
+  companyFeatures: report.features,
+  companyIdealTalent: report.ideal_talent,
+  companyNews: report.news,
+});
+
+const map_to_industryDetailDTO = (report: IndustryReport): IndustryDetailDTO => ({
+  industryType: report.type,
+  industryFeatures: report.features,
+  industryNews: report.news,
+});
