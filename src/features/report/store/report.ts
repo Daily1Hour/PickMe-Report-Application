517aef105cb62981318b04d9e4abcdb43b85cb4f
NarciSource <jwch11@gmail.com>
2025-01-28 17:59:42 +0900

    refactor: 스키마 동적 정의 스토어로 이동

--- src/features/report/store/report.ts
+++ src/features/report/store/report.ts
@@ -2,8 +2,9 @@ import { computed, shallowRef } from "vue";
 import { defineStore } from "pinia";
 import { useRoute } from "vue-router";
 
-import { ReportType } from "@/entities/report/model";
 import { Category } from "@/shared/model";
+import { CompanyReport, IndustryReport, ReportType } from "@/entities/report/model";
+import { companySchema, industrySchema } from "../model";
 
 // 중앙 상태 저장소
 export const useReportStore = defineStore("report", () => {
@@ -13,5 +14,16 @@ export const useReportStore = defineStore("report", () => {
   const category = computed(() => (route.query.category as Category) || Category.Company);
   const report = shallowRef<ReportType>({} as ReportType);
 
-  return { report, category, id };
+  // 스키마 동적 정의
+  const schema = computed(() => {
+    if (report.value instanceof CompanyReport) {
+      return companySchema;
+    }
+    if (report.value instanceof IndustryReport) {
+      return industrySchema;
+    }
+    throw new Error("Invalid report type");
+  });
+
+  return { id, category, report, schema };
 });
--- src/features/report/ui/form-layout.vue
+++ src/features/report/ui/form-layout.vue
@@ -7,46 +7,31 @@
 </template>
 
 <script setup lang="ts">
-import { computed, ref, watch } from "vue";
+import { ref, watch } from "vue";
 import { storeToRefs } from "pinia";
 import { useForm } from "vee-validate";
 import { toTypedSchema } from "@vee-validate/zod";
 
-import { CompanyReport, IndustryReport } from "@/entities/report/model";
-import { companySchema, industrySchema, ReportSchemaType } from "../model";
+import { ReportSchemaType } from "../model";
+import { map_form_to_report, map_report_to_form } from "../service";
 import { useReportStore } from "../store/report";
-import { map_report_to_form, map_form_to_report } from "../service";
 import ActionsReport from "./actions-report.vue";
 
-// 상태 저장소에서 현재 report 데이터 읽음
-const { report } = storeToRefs(useReportStore());
+const { report, schema } = storeToRefs(useReportStore());
 
-// 스키마 동적 정의
-const schema = computed(() => {
-  if (report.value instanceof CompanyReport) {
-    return companySchema;
-  }
-  if (report.value instanceof IndustryReport) {
-    return industrySchema;
-  }
-  throw new Error("Invalid report type");
-});
 // 폼 정의
 const { handleSubmit, setValues, meta } = useForm<ReportSchemaType>({
   validationSchema: toTypedSchema(schema.value),
-  // 엔터티 모델 → 폼 모델
-  initialValues: map_report_to_form(report.value),
+  initialValues: map_report_to_form(report.value), // 엔터티 모델 → 폼 모델
 });
 
 // 폼 갱신
 watch(report, (new_report) => setValues(map_report_to_form(new_report)), { immediate: true });
 
-// 액션 컴포넌트 참조
-const actions_report = ref();
-// 폼 제출 핸들러
+// 폼 제출
+const actions_report = ref(); // 액션 컴포넌트 참조
 const onSubmit = handleSubmit((values: ReportSchemaType) => {
-  // 폼 모델 → 엔터티 모델
-  const report = map_form_to_report(values);
+  const report = map_form_to_report(values); // 폼 모델 → 엔터티 모델
   actions_report.value.onSubmit(report);
 });
 </script>
