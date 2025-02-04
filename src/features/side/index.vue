b9ee88c251490cb252ce2bb90ff5759c780e3cc8
NarciSource <jwch11@gmail.com>
2025-02-04 22:47:41 +0900

    feat: 사이드바 스켈레톤 로딩 상태 적용

--- src/features/side/index.vue
+++ src/features/side/index.vue
@@ -1,11 +1,27 @@
 <template>
-  <load-summaries />
-
   <drawer-layout>
-    <display-summaries />
+    <load-summaries />
+
+    <skeleton-transition v-model="show_skeleton">
+      <template #skeleton>
+        <display-summaries-skeleton />
+      </template>
+      <template #content>
+        <display-summaries />
+      </template>
+    </skeleton-transition>
   </drawer-layout>
 </template>
 
 <script setup lang="ts">
-import { LoadSummaries, DisplaySummaries, DrawerLayout } from "./ui";
+import { computed } from "vue";
+import { storeToRefs } from "pinia";
+
+import { useSummaryStore } from "./store";
+import { SkeletonTransition } from "@/features/report/ui";
+import { LoadSummaries, DisplaySummaries, DisplaySummariesSkeleton, DrawerLayout } from "./ui";
+
+const { summaries } = storeToRefs(useSummaryStore());
+
+const show_skeleton = computed(() => !summaries.value);
 </script>
