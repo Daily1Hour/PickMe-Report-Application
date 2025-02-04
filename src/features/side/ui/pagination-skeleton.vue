c257e258b4b53a890bf965e205637eb49a6e54cb
NarciSource <jwch11@gmail.com>
2025-02-04 22:44:24 +0900

    feat: 사이드바 스켈레톤 컴포넌트 추가

---
+++ src/features/side/ui/pagination-skeleton.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex flex-center" title="페이지 이동">
+    <q-icon name="first_page" class="q-mx-xs" color="grey-4" size="1.715em" />
+    <q-icon name="keyboard_arrow_left" class="q-mx-xs" color="grey-4" size="1.715em" />
+    <q-skeleton color="grey-4" width="30%" />
+    <q-icon name="keyboard_arrow_right" class="q-mx-xs" color="grey-4" size="1.715em" />
+    <q-icon name="last_page" class="q-mx-xs" color="grey-4" size="1.715em" />
+  </div>
+</template>
