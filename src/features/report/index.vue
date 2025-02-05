b790a8e8a905a9866c62c45a45778a63e4a56f2a
NarciSource <jwch11@gmail.com>
2025-02-05 21:57:46 +0900

    refactor: 스켈레톤 전환 컴포넌트 분리

--- src/features/report/index.vue
+++ src/features/report/index.vue
@@ -2,17 +2,16 @@
   <div class="column q-mx-auto" style="max-width: 1024px">
     <load-report />
 
-    <transition name="fade" mode="out-in">
-      <div v-if="show_skeleton" key="skeleton">
+    <skeleton-transition v-model="show_skeleton">
+      <template #skeleton>
         <navigation-skeleton />
         <display-report-skeleton />
-      </div>
-
-      <div v-else key="content">
+      </template>
+      <template #content>
         <navigation />
         <display-report />
-      </div>
-    </transition>
+      </template>
+    </skeleton-transition>
   </div>
 </template>
 
@@ -27,21 +26,10 @@ import {
   NavigationSkeleton,
   DisplayReport,
   DisplayReportSkeleton,
+  SkeletonTransition,
 } from "./ui";
 
 const { report } = storeToRefs(useReportStore());
 
 const show_skeleton = computed(() => !report.value);
 </script>
-
-<style scoped>
-/* 페이드 애니메이션 */
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.3s ease-in-out;
-}
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-}
-</style>
---
+++ src/features/report/ui/skeleton-transition.vue
@@ -0,0 +1,27 @@
+<template>
+  <transition name="fade" mode="out-in">
+    <div v-if="show_skeleton" key="skeleton">
+      <slot name="skeleton" />
+    </div>
+
+    <div v-else key="content">
+      <slot name="content" />
+    </div>
+  </transition>
+</template>
+
+<script setup lang="ts">
+const show_skeleton = defineModel<boolean>({ required: true });
+</script>
+
+<style scoped>
+/* 페이드 애니메이션 */
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease-in-out;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
