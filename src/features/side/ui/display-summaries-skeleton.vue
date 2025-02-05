20fd5fae5fa947aaaa256bb4e32e7b75111df1a1
NarciSource <jwch11@gmail.com>
2025-02-05 22:44:24 +0900

    feat: 사이드바 스켈레톤 컴포넌트 추가

---
+++ src/features/side/ui/display-summaries-skeleton.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-list separator>
+    <q-item clickable v-ripple>
+      <add-report-skeleton />
+    </q-item>
+
+    <q-item v-for="index in 5" :key="index" clickable v-ripple>
+      <section-tab-skeleton />
+    </q-item>
+  </q-list>
+
+  <pagination-skeleton />
+</template>
+
+<script setup lang="ts">
+import AddReportSkeleton from "./add-report-skeleton.vue";
+import SectionTabSkeleton from "./section-tab-skeleton.vue";
+import PaginationSkeleton from "./pagination-skeleton.vue";
+</script>
