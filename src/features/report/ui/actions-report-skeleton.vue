9bec9b0a56d691a306791e9dffd07de5f037161c
NarciSource <jwch11@gmail.com>
2025-02-05 19:18:56 +0900

    feat: 액션 스켈레톤 컴포넌트 추가

---
+++ src/features/report/ui/actions-report-skeleton.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="fixed-top-right q-ma-md row justify-end q-gutter-sm">
+    <q-skeleton type="QBtn" animation="fade" title="삭제" width="60px" />
+    <q-skeleton type="QBtn" animation="fade" title="저장" width="60px" />
+  </div>
+</template>
