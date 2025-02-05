ef3090ba22ae33ac2ddd713a4e1c9f3db30e5256
NarciSource <jwch11@gmail.com>
2025-02-05 11:07:43 +0900

    refactor: 섹션 필드 컴포넌트 분리

---
+++ src/features/report/ui/section-title-field.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-input
+    v-model="model_value"
+    class="q-px-xl text-h5"
+    :label="label"
+    :placeholder="label + '을 입력해주세요.'"
+    :error-message="error_message"
+    :error="!!error_message"
+  />
+</template>
+
+<script setup lang="ts">
+const { label, error_message } = defineProps<{ label: string; error_message: any }>(); // 프로퍼티
+const model_value = defineModel<any>({ required: true }); // 섹션 데이터
+</script>
