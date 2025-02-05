bbbaaa7e9161e2a21103f1538668ee35a27c891e
NarciSource <jwch11@gmail.com>
2025-02-05 21:51:34 +0900

    design: 스켈레톤 페이드 애니메이션 추가

--- src/features/report/index.vue
+++ src/features/report/index.vue
@@ -2,15 +2,22 @@
   <div class="column q-mx-auto" style="max-width: 1024px">
     <load-report />
 
-    <navigation v-if="!!report" />
-    <navigation-skeleton v-else />
+    <transition name="fade" mode="out-in">
+      <div v-if="show_skeleton" key="skeleton">
+        <navigation-skeleton />
+        <display-report-skeleton />
+      </div>
 
-    <display-report v-if="!!report" />
-    <display-report-skeleton v-else />
+      <div v-else key="content">
+        <navigation />
+        <display-report />
+      </div>
+    </transition>
   </div>
 </template>
 
 <script setup lang="ts">
+import { computed } from "vue";
 import { storeToRefs } from "pinia";
 
 import { useReportStore } from "./store";
@@ -23,4 +30,18 @@ import {
 } from "./ui";
 
 const { report } = storeToRefs(useReportStore());
+
+const show_skeleton = computed(() => !report.value);
 </script>
+
+<style scoped>
+/* 페이드 애니메이션 */
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease-in-out;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
