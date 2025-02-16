321e0d97766989435875980b8f410e57f9698c74
NarciSource <jwch11@gmail.com>
2025-02-16 23:31:05 +0900

    chore: module federation 추가 및 구성

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,20 @@ import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import vue from "@vitejs/plugin-vue";
 import { quasar, transformAssetUrls } from "@quasar/vite-plugin";
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
     // 환경변수 증설
     process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
-    // 서버포트
-    const serverPort = Number(process.env.VITE_SERVER_PORT);
+    const serverPort = Number(process.env.VITE_SERVER_PORT); // 서버 포트
+    const styleguideUrl = process.env.VITE_STYLEGUIDE_URL!; // 스타일가이드 URL
 
     // vite 설정
     return {
         plugins: [
-            tsconfigPaths({
-                loose: true,
-            }), // tsconfig.json의 paths 설정을 적용
+            tsconfigPaths({ loose: true }), // tsconfig.json의 paths 설정을 적용
             vue({
                 // Vue 플러그인 적용
                 template: {
@@ -25,6 +24,13 @@ export default defineConfig(({ mode }) => {
                 },
             }),
             quasar(), // Quasar 플러그인 적용
+            federation({
+                // 모듈 페더레이션 적용
+                remotes: {
+                    "@styleguide": styleguideUrl,
+                },
+                shared: ["vue"],
+            }),
         ],
         build: {
             target: "esnext",
